f9f810f853d293dd1da5586357a3a2218fbc673e
Michael Gale <mgale01@gmail.com>
2022-07-27 22:12:09 -0500

    fixed typo in comment

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1905,9 +1905,11 @@
 
   /**
    * Enable the G26 Mesh Validation Pattern tool.
+   */
    //enabled by mike
    
   #define G26_MESH_VALIDATION
+
   #if ENABLED(G26_MESH_VALIDATION)
     #define MESH_TEST_NOZZLE_SIZE    0.4  // (mm) Diameter of primary nozzle.
     #define MESH_TEST_LAYER_HEIGHT   0.2  // (mm) Default layer height for G26.
@@ -1919,6 +1921,7 @@
   #endif
 
 #endif
+
 
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
